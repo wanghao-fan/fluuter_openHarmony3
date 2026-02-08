b58af4627573fc2036f606fe525cec64b1743f05
whao <wanghao@mac.com>
2026-02-08 20:00:35 +0800

    68

--- lib/components/size_transition.dart
+++ lib/components/size_transition.dart
@@ -1,72 +1,94 @@
 import 'package:flutter/material.dart';
-import 'components/size_transition.dart';
 
-void main() {
-  runApp(const MyApp());
+class SizeTransitionWidget extends StatefulWidget {
+  final Widget child;
+  final Axis axis;
+  final double axisAlignment;
+  final Duration duration;
+  final Curve curve;
+  final Function()? onTap;
+
+  const SizeTransitionWidget({
+    Key? key,
+    required this.child,
+    this.axis = Axis.vertical,
+    this.axisAlignment = 0.0,
+    this.duration = const Duration(milliseconds: 300),
+    this.curve = Curves.easeInOut,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  State<SizeTransitionWidget> createState() => _SizeTransitionWidgetState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _SizeTransitionWidgetState extends State<SizeTransitionWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool _isExpanded = false;
 
-  // This widget is the root of your application.
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    )..value = 1.0; // 初始状态为展开
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: widget.curve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleSize() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter for openHarmony',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return GestureDetector(
+      onTap: () {
+        _toggleSize();
+        if (widget.onTap != null) {
+          widget.onTap!();
+        }
+      },
+      child: SizeTransition(
+        sizeFactor: _animation,
+        axis: widget.axis,
+        axisAlignment: widget.axisAlignment,
+        child: widget.child,
       ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Flutter for openHarmony'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SizeTransitionExample extends StatefulWidget {
+  const SizeTransitionExample({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SizeTransitionExample> createState() => _SizeTransitionExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SizeTransitionExampleState extends State<SizeTransitionExample> {
+  int _clickCount = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _clickCount++;
     });
   }
 
@@ -74,14 +96,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter for OpenHarmony 实战'),
+        title: Text('大小变化过渡动画示例'),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Text(
               '大小变化过渡动画示例',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
@@ -134,11 +156,11 @@ class _MyHomePageState extends State<MyHomePage> {
               curve: Curves.elasticOut,
               onTap: _incrementCounter,
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 40),
 
             // 点击计数显示
             Text(
-              '点击计数: $_counter',
+              '点击计数: $_clickCount',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
